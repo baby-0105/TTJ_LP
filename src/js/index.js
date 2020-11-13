b7972d423d567d8fdf87596f0d1b0073fcbe9e42
山崎淳平 <>
2020-11-13 20:18:52 +0900

    add animation, modifier index.ejs and home.scss

--- src/js/index.js
+++ src/js/index.js
@@ -10,8 +10,8 @@ $(() => {
 
   // スクロールイベント
   $(window).scroll(function() {
-    let windowHeight = $(window).height(),
-        scrollPosi = $(window).scrollTop();
+    let windowHeight = $(window).height()   ,
+        scrollPosi   = $(window).scrollTop();
 
     // underline：伸びる & TTJロゴ：fadeIn
     $('#aboutService').each(function() {
@@ -28,19 +28,21 @@ $(() => {
         }
       }
     })
+
+    $('#container').each(function() {
+      let aboutServicePosi = $(this).offset().top;
+
+      $(this).css('visibility','hidden');
+
+      if (scrollPosi > aboutServicePosi - windowHeight + 150){
+        for(let i=0; i<8; i++) {
+          let delayTime = ['0', '500', '1000', '1500', '2000', '2500', '3000', '3500'];
+
+          $(`#fade-in-down${i+1}`).delay(delayTime[i]).queue(function() {
+            $(this).addClass('fadeInDown');
+          })
+        }
+      }
+    })
   });
-
-  $('#fade-in-up').css('visibility','hidden');
-$(window).scroll(function(){
- var windowHeight = $(window).height(),
-     topWindow    = $(window).scrollTop();
-
- $('#fade-in-up').each(function(){
-  var targetPosition = $(this).offset().top;
-  
-  if(topWindow > targetPosition - windowHeight + 100){
-   $(this).addClass("fadeInDown");
-  }
- });
-});
 })
